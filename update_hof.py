fbc76f1eaf9801a05246c6f529fa78eb3916f03f
bgd <trifan.bogdan09@yahoo.com>
2025-06-30 20:13:32 +0300

    Add files via upload

---
+++ update_hof.py
@@ -0,0 +1,105 @@
+from time import sleep
+import requests
+import re
+
+
+TOKEN = ''
+SLEEP=2
+HEADERS = {
+    'Authorization': 'Bearer ' + TOKEN,
+    'Content-Type': 'application/json'
+}
+
+def get_other_titles(repo_link):
+    repo_link = repo_link.split('/')
+    owner = repo_link[3]
+    repo = repo_link[4]
+
+    response = requests.get(f'https://api.github.com/repos/{owner}/{repo}/branches', headers=HEADERS)
+    json = response.json()
+
+    branches = []
+    for item in json:
+        branches.append(item['name'])
+
+    aliases = []
+    blacklist = ["tema", "surse", "fonts", "assets", "sources", "Description", "How to Play",
+                 "Instrucțiuni de compilare", "OOP Template", "Proiect POO",
+                 "Nu primesc notă pentru că nu am pus titlu și descriere"]
+    for branch in branches:
+        response = requests.get(f'https://raw.githubusercontent.com/{owner}/{repo}/{branch}/README.md', headers=HEADERS)
+        lines = response.text.splitlines()
+
+        for line in lines:
+            alias = None
+            if line.startswith('# '):
+                alias = line[2:]
+            elif line.startswith('## '):
+                alias = line[3:]
+
+            skip = False
+            if alias is not None and alias not in aliases:
+                for banned_word in blacklist:
+                    if banned_word.lower() in line.lower():
+                        skip = True
+                        break
+                if skip:
+                    continue
+                aliases.append(alias)
+
+        sleep(SLEEP)
+
+    return aliases
+
+
+with open('./HoF.md', encoding='utf-8') as f:
+    hof = f.read()
+
+print('Finished reading HoF.md')
+
+document_lines = []
+
+for line in hof.split('\n'):
+    match = re.search(r'https://github.com/[^\s\)\]]+', line)
+    is_available = True
+
+    if match:
+        repo_link = match.group(0)
+        line = line.replace('|', '')
+
+        print(f'============== {repo_link} ===============')
+
+        try:
+            response = requests.get(repo_link, allow_redirects=True, timeout=15)
+
+            if response.status_code != 200:
+                print(f'{line} is not available.')
+
+                if '~' not in line: # daca nu a fost deja taiat
+                    line = '~' + line + '~'
+            else:
+                if response.url != repo_link:
+                    print(f"Repo-ul {repo_link} a fost mutat la {response.url}")
+                    line = line.replace(repo_link, response.url)
+                print(f'{line} is available.')
+
+                available_titles = get_other_titles(repo_link)
+                print('Available titles: ', end='')
+                for alias in available_titles:
+                    print(f'{alias} | ', end='')
+                print('')
+
+            line = line.rstrip(' ')
+            line = line + ' | '
+
+        except Exception as e:
+            print(f'Eroare la repo-ul {repo_link}')
+            print(e.with_traceback())
+        sleep(SLEEP)
+
+        print('=======================================\n')
+
+    document_lines.append(line)
+
+with open('./HoF.md', 'w', encoding='utf-8') as f:
+    f.write('\n'.join(document_lines))
