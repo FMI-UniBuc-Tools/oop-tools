30e7c2dc61271398fbea57945e9e9cd4295d22cc
bgd <trifan.bogdan09@yahoo.com>
2025-07-02 15:52:30 +0300

    make only 1 request instead of 2

--- update_hof.py
+++ update_hof.py
@@ -12,44 +12,33 @@ HEADERS = {
     'Content-Type': 'application/json'
 }
 
-def get_other_titles(repo_link):
-    repo_link = repo_link.split('/')
-    owner = repo_link[3]
-    repo = repo_link[4]
+def get_other_titles(response):
 
-    url = f'https://api.github.com/repos/{owner}/{repo}/contents/README.md'
-    response = requests.get(url, headers=HEADERS)
     aliases = []
-
-    response = response.json()
 
     if "content" not in response:
         return aliases
 
-    response = base64.b64decode(response["content"]).decode('utf-8').splitlines()
+    content_lines = base64.b64decode(response["content"]).decode('utf-8').splitlines()
 
-    aliases = []
-    blacklist = ["tema", "surse", "fonts", "assets", "sources", "Description", "How to Play",
-                 "Instrucțiuni de compilare", "OOP Template", "Proiect POO", "OOP-Project", "OOP-Template",
-                 "Nu primesc notă pentru că nu am pus titlu și descriere",
-                 "instru"]
+    blacklist = [
+        "tema", "surse",  #(re)surse
+        "fonts", "assets", "sources", # (re)sources
+        "Description", "How to Play",
+        "Instrucțiuni de compilare", "OOP Template", "Proiect POO", "OOP-Project", "OOP-Template",
+        "Nu primesc notă pentru că nu am pus titlu și descriere",
+        "instru" # (instru)ctiuni / (instru)ctions
+        "Prerequisites", "API", "Download", "Important", "Update"
+    ]
 
-    for line in response:
-        alias = None
-        if line.startswith('# '):
-            alias = line[2:]
-        elif line.startswith('## '):
-            alias = line[3:]
-
-        skip = False
-        if alias is not None and alias not in aliases:
-            for banned_word in blacklist:
-                if banned_word.lower() in line.lower():
-                    skip = True
-                    break
-            if skip:
-                continue
-            aliases.append(alias)
+    for line in content_lines:
+        match = re.match(r'^#{1,2} (.+)', line)
+        if match:
+            alias = match.group(1).strip()
+            if alias not in aliases:
+                if any(banned.lower() in alias.lower() for banned in blacklist):
+                    continue
+                aliases.append(alias)
 
     return aliases
 
@@ -63,29 +52,64 @@ document_lines = []
 
 for line in hof.split('\n'):
     match = re.search(r'https://github.com/[^\s\)\]]+', line)
+    """
+    url = {
+        name: string,
+        path: string,
+        sha: string,
+        size: int,
+        url: string,
+        html_url: string,
+        git_url: string,
+        download_url: string,
+        type: string,
+        content: string,
+        encoding: string,
+        _links {
+            self: string,
+            git: string,
+            html: string,
+        }
+    }
+    """
+
     is_available = True
 
     if match:
         repo_link = match.group(0)
+        url = f'https://api.github.com/repos/{repo_link.replace('https://github.com/', '')}/contents/README.md'
         line = line.rstrip('?| ')
 
         print(f'============== {repo_link} ===============')
 
         try:
-            response = requests.get(repo_link, allow_redirects=True, timeout=15)
+            response = requests.get(url, allow_redirects=True, timeout=15, headers=HEADERS)
+            response = response.json()
 
-            if response.status_code != 200:
-                print(f'{line} is not available.')
+            if 'message' in response and response['message'] == 'Not Found': # Nu a fost gasit README in repo
+                # mai fac un request pentru a verifica daca repo-ul exista
+                response = requests.get(f'https://api.github.com/repos/{repo_link.replace('https://github.com/', '')}',
+                                        allow_redirects=True,
+                                        timeout=15,
+                                        headers=HEADERS)
 
-                if '~' not in line: # daca nu a fost deja taiat
-                    line = '~' + line + '~'
+                if response.status_code != 200:
+                    print(f'{line} is not available.')
+
+                    if '~' not in line: # daca nu a fost deja taiat
+                        line = '~' + line + '~'
+                else:
+                    print('README.md not found')
             else:
-                if response.url != repo_link:
-                    print(f"Repo-ul {repo_link} a fost mutat la {response.url}")
-                    line = line.replace(repo_link, response.url)
+                html_url = response['html_url']
+                html_url = re.sub(r'/blob/[^/]+/README\.md$', '', html_url)
+
+                if repo_link != html_url:
+                    print(f"Repo-ul {repo_link} a fost mutat la {html_url}")
+                    line = line.replace(repo_link, html_url)
                 print(f'{line} is available.')
 
-                available_titles = get_other_titles(repo_link)
+                available_titles = get_other_titles(response)
                 print('Available titles: ', end='')
                 for alias in available_titles:
                     print(f'{alias} | ', end='')
@@ -96,9 +120,10 @@ for line in hof.split('\n'):
         except Exception as e:
             print(f'Eroare la repo-ul {repo_link}')
             print(e.with_traceback())
-        sleep(SLEEP)
 
         print('=======================================\n')
+
+        sleep(SLEEP)
 
     document_lines.append(line)
 
