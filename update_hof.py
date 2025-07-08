0d4d07f079aabab9f6c443fa66e6f900834f108a
bgd <trifan.bogdan09@yahoo.com>
2025-07-08 10:04:35 +0300

    - pastrata spatierea initiala - se adauga ? in loc de | la ultimul proiect dintr-un an - nu se mai afiseaza detalii despre repo daca titlul se afla in "Available titles" - nu se mai afiseaza "Available titles" daca lista este goala

--- update_hof.py
+++ update_hof.py
@@ -2,18 +2,18 @@ from time import sleep
 import requests
 import re
 import base64
+from datetime import datetime
 
 
-
-TOKEN = ''
-SLEEP=1.5
+TOKEN = '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'
+SLEEP = 1.5
 HEADERS = {
     'Authorization': 'Bearer ' + TOKEN,
     'Content-Type': 'application/json'
 }
+CURRENT_YEAR = str(datetime.now().year)
 
 def get_other_titles(response):
-
     aliases = []
 
     if "content" not in response:
@@ -28,7 +28,8 @@ def get_other_titles(response):
         "Instrucțiuni de compilare", "OOP Template", "Proiect POO", "OOP-Project", "OOP-Template",
         "Nu primesc notă pentru că nu am pus titlu și descriere",
         "instru" # (instru)ctiuni / (instru)ctions
-        "Prerequisites", "API", "Download", "Important", "Update"
+        "Prerequisites", "API", "Download", "Important", "Update",
+        "OOP_Project"
     ]
 
     for line in content_lines:
@@ -49,6 +50,7 @@ with open('./HoF.md', encoding='utf-8') as f:
 print('Finished reading HoF.md')
 
 document_lines = []
+is_current_year = False
 
 for line in hof.split('\n'):
     match = re.search(r'https://github.com/[^\s\)\]]+', line)
@@ -75,10 +77,18 @@ for line in hof.split('\n'):
 
     is_available = True
 
+    line_year_match = re.search(r'\*\*\d{4}-(\d{4})\*\*', line)
+    if line_year_match and line_year_match.group(1) == CURRENT_YEAR and is_current_year == False:
+        is_current_year = True
+
     if match:
         repo_link = match.group(0)
         url = f'https://api.github.com/repos/{repo_link.replace('https://github.com/', '')}/contents/README.md'
         line = line.rstrip('?| ')
+
+        project_name = re.search(r'\[([^\]]+)\]', line)
+        if project_name:
+            project_name = project_name.group(1)
 
         print(f'============== {repo_link} ===============')
 
@@ -97,7 +107,7 @@ for line in hof.split('\n'):
                     print(f'{line} is not available.')
 
                     if '~' not in line: # daca nu a fost deja taiat
-                        line = '~' + line + '~'
+                        line = '~' + line + '~ (dead link)'
                 else:
                     print('README.md not found')
             else:
@@ -110,10 +120,22 @@ for line in hof.split('\n'):
                 print(f'{line} is available.')
 
                 available_titles = get_other_titles(response)
-                print('Available titles: ', end='')
-                for alias in available_titles:
-                    print(f'{alias} | ', end='')
-                print('')
+
+                print_titles = True
+
+                if len(available_titles) == 0:
+                    print_titles = False
+
+                for title in available_titles:
+                    if project_name.lower() in title.lower():
+                        print_titles = False
+                        break
+
+                if print_titles:
+                    print('Available titles: ', end='')
+                    for alias in available_titles:
+                        print(f'{alias} | ', end='')
+                    print('')
 
             line = line + ' | '
 
@@ -125,7 +147,17 @@ for line in hof.split('\n'):
 
         sleep(SLEEP)
 
+    line = line.rstrip(' ')
+    if line == '':
+        if document_lines[-1] != '':
+            if is_current_year == False:
+                document_lines[-1] = document_lines[-1].rstrip('|')
+            else:
+                document_lines[-1] = document_lines[-1] + ' ?'
+                is_current_year = False
+
     document_lines.append(line)
+
 
 with open('./HoF.md', 'w', encoding='utf-8') as f:
     f.write('\n'.join(document_lines))
